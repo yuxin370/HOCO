9b8058cff99f64cd039a15f240f9a47e674dcece
WittenYeh <2021201613@ruc.edu.cn>
2023-09-25 22:07:29 +0800

    init TADOC compress framework

---
+++ postgresql-16.0/src/common/tadoc_compress.c
@@ -0,0 +1,20 @@
+/*-------------------------------------------------
+
+TADOC COMPRESS FUNCTION:
+
+
+
+--------------------------------------------------*/
+
+
+
+
+#ifndef FRONTEND
+#include "postgres.h"
+#else
+#include "postgres_fe.h"
+#endif
+
+#include <limits.h>
+
+#include "common/tadoc_compress.h"
---
+++ postgresql-16.0/src/include/common/tadoc_compress.h
@@ -0,0 +1,6 @@
+#ifndef _TADOC _COMPRESS_H_
+#define _TADOC_COMPRESS_H_
+
+
+
+#endif  /* _TADOC_COMPRESS_H_ */
